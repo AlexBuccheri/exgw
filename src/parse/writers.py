d5d158361eefe3a596d19148322e790f47460ce9
Alex Buccheri <alexanderbuccheri@googlemail.com>
2022-03-17 22:08:20 +0100

    Serialised data written to XML file string

--- src/parse/writers.py
+++ src/parse/writers.py
@@ -1,47 +1,12 @@
 """ Write to file
 """
+from typing import List
+
 
 def write_species_file_from_dict(species: dict) -> str:
     """
     Expect dict with top-level keys {'species', 'muffin_tin', 'atomic_states', 'basis'}
-
-    <?xml version="1.0" encoding="UTF-8"?>
-<spdb xsi:noNamespaceSchemaLocation="../../xml/species.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-  <sp chemicalSymbol="Ti" name="titanium" z="-22.0000" mass="87256.20311">
-    <muffinTin rmin="0.100000E-05" radius="2.0000" rinf="25.7965" radialmeshPoints="600"/>
-    <atomicState n="1" l="0" kappa="1" occ="2.00000" core="true"/>
-    <atomicState n="2" l="0" kappa="1" occ="2.00000" core="true"/>
-    <atomicState n="2" l="1" kappa="1" occ="2.00000" core="true"/>
-    <atomicState n="2" l="1" kappa="2" occ="4.00000" core="true"/>
-    <atomicState n="3" l="0" kappa="1" occ="2.00000" core="false"/>
-    <atomicState n="3" l="1" kappa="1" occ="2.00000" core="false"/>
-    <atomicState n="3" l="1" kappa="2" occ="4.00000" core="false"/>
-    <atomicState n="3" l="2" kappa="2" occ="2.00000" core="false"/>
-    <atomicState n="4" l="0" kappa="1" occ="2.00000" core="false"/>
-    <basis>
-      <default type="lapw" trialEnergy="0.1500" searchE="false"/>
-
-      <custom l="0" type="lapw" trialEnergy="-1.83220324630948" searchE="false"/>
-      <lo l="0">
-        <wf matchingOrder="0" trialEnergy="-1.83220324630948" searchE="false"/>
-        <wf matchingOrder="1" trialEnergy="-1.83220324630948" searchE="false"/>
-      </lo>
-      <lo l="0">
-        <wf matchingOrder="1" trialEnergy="-1.83220324630948" searchE="false"/>
-        <wf matchingOrder="2" trialEnergy="-1.83220324630948" searchE="false"/>
-      </lo>
-      <lo l="0">
-        <wf matchingOrder="0" trialEnergy="-1.83220324630948" searchE="false"/>
-        <wf matchingOrder="0" trialEnergy="3.48339510536654" searchE="false"/>
-      </lo>
-      <lo l="0">
-        <wf matchingOrder="0" trialEnergy="3.48339510536654" searchE="false"/>
-        <wf matchingOrder="1" trialEnergy="3.48339510536654" searchE="false"/>
-      </lo>
-        </basis>
-  </sp>
-</spdb>
-
+    TODO(Alex) Document
     :param species:
     :return:
     """
@@ -66,57 +31,92 @@ def write_species_file_from_dict(species: dict) -> str:
     # Atomic states
     species_str += atomic_states_string(species['atomic_states'])
 
-
-    # Presort apws into l-channels
+    l_values = [lapw['l'] for lapw in species['basis']['custom']]
+    l_max = max(l_values)
 
     # Presort LOs into l-channels
+    all_los = species['basis']['lo']
+    los_by_lvalue = [[] for _ in range(0, l_max + 1)]
+    for lo in all_los:
+        l = lo['l']
+        los_by_lvalue[l].append(lo)
 
-    # "<basis>\n "
-    # species_str += function_string(species['default'])
-    # for l in range(0, l_max +1):
-    #     species_str += function_string(species['custom'][l])
-    #     species_str += local_orbitals_string(species['custom'][l])
+    species_str += "    <basis>\n "
 
-    species_str += """  </basis>
+    # Default l(apw) line
+    type = species['basis']['default'][0]['type']
+    trial_energy = species['basis']['default'][0]['trialEnergy']
+    search_e  = species['basis']['default'][0]['searchE']
+    species_str += f'      <default type="{type}" trialEnergy="{trial_energy}" searchE="{search_e}"/> \n\n'
+
+    # lapw and any explicit local orbitals, per l-channel
+    # Note, there should only be one l(apw) per l-channel
+    print(species['basis']['custom'])
+    for l in range(0, l_max +1):
+        species_str += function_string(species['basis']['custom'][l])
+        species_str += local_orbitals_string(los_by_lvalue[l])
+
+    species_str += """    </basis>
   </sp>
 </spdb>"""
 
     return species_str
 
 
-def local_orbitals_string():
+def local_orbitals_string(los_l: List[dict]) -> str:
     """
+    TODO(Alex) Document
     Expect
     {'l': 2, 'matchingOrder': [0, 1], 'trialEnergy': [0.0, 0.0], 'searchE': [False, False]},
            {'l': 2, 'matchingOrder': [1, 2], 'trialEnergy': [0.0, 0.0], 'searchE': [False, False]},
     :return:
     """
-    pass
+    string = ''
+    for lo in los_l:
+        string += local_orbital_string(lo)
+    return string + '\n'
 
 
-def local_orbital_string():
+def local_orbital_string(lo: dict) -> str:
     """
+    TODO(Alex) Document
+    Generate a string of the form:
+    <lo l="2">
+	   <wf matchingOrder="0" trialEnergy="0.00" searchE="false"/>
+       <wf matchingOrder="1" trialEnergy="0.00" searchE="false"/>
+    </lo>
 
     :return:
     """
-    pass
+    l = lo['l']
+    string = f'      <lo l="{l}">\n'
+    n_radial_functions = len(lo['matchingOrder'])
+    for ir in range(0, n_radial_functions):
+        matchingOrder = lo['matchingOrder'][ir]
+        trialEnergy = lo['trialEnergy'][ir]
+        searchE = str(lo['searchE'][ir]).lower()
+        string += f'       <wf matchingOrder="{matchingOrder}" trialEnergy="{trialEnergy}" searchE="{searchE}"/>\n'
+    string += '      </lo>\n'
+    return string
 
 
 def function_string(function: dict):
     """
-
+    TODO(Alex) Document
     :param function:
     :return:
     """
-    type = str(function['type'][0])
-    trial_energy = str(function['trialEnergy'][0])
-    search_e = str(function['searchE'][0]).lower()
-    string = f'<default type="{type}" trialEnergy="{trial_energy}" searchE="{search_e}"/> \n'
+    l = function['l']
+    type = function['type']
+    trial_energy = str(function['trialEnergy'])
+    search_e = str(function['searchE']).lower()
+    string = f'      <custom l="{l}" type="{type}" trialEnergy="{trial_energy}" searchE="{search_e}"/> \n\n'
     return string
 
 
 def atomic_states_string(atomic_states: dict) -> str:
     """
+    TODO(Alex) Document
     :param atomic_states:
     :return:
     """
