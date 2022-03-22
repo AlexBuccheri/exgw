43b3f510f204242ecfcaac57c0ee4196660d9341
Alex Buccheri <alexanderbuccheri@googlemail.com>
2022-03-22 21:30:10 +0100

    TiO2 ready to test

---
+++ tio2/lo_spread/calculations.py
@@ -0,0 +1,216 @@
+""" LO Sweep calculations
+
+For TiO2, want to run l_max = (4,3) (5,4) (6,5) (7,6)
+with same cut-off per l_max channel: [20, 30, 40, 50, 60, 70, 80, 90, 100]
+Hence 36 calculations.
+"""
+from typing import Optional
+from collections import OrderedDict
+
+from src.basis.optimised_basis import construct_optimised_basis
+from src.job_schedulers import slurm
+
+
+class Calculation:
+    def __init__(self, directory: str,
+                 lo_energy_cutoff: Optional[dict] = None,
+                 basis: Optional[dict] = None,
+                 run_script: Optional[str] = None):
+        self.directory = directory
+        self.variable = lo_energy_cutoff
+        self.basis = basis
+        self.run_script = run_script
+
+
+# Specify which cluster to generate a run script for
+cluster = 'dune'
+
+if cluster is 'dune':
+    default_env_vars = OrderedDict([('EXE', '/users/sol/abuccheri/exciting/bin/excitingmpismp'),
+                                    ('OUT', 'terminal.out')])
+    default_module_envs = ['intel/2019']
+    default_directives = slurm.set_slurm_directives(job_name="gw_tio2",
+                                                    time=[0, 24, 0, 0],
+                                                    partition='all',
+                                                    exclusive=True,
+                                                    nodes=4,
+                                                    ntasks_per_node=2,
+                                                    cpus_per_task=18,
+                                                    hint='nomultithread')
+    set_run_script = slurm.set_slurm_script
+elif cluster is 'hawk':
+    raise NotImplementedError('Need to specify settings for HAWK environment')
+else:
+    raise ValueError('Choice of HPC machine is not valid: {}')
+
+
+def calc_lmax43_locut20(default_basis, lo_recommendations) -> Calculation:
+    """ Define a calculation.
+
+    * relative directory (to root)
+    * basis settings in dictionary form
+    * Note, lo_energy_cutoff assigned to variable is not much good. This could be via **kwargs
+
+    Using same cutoff for each channel.
+    :return: Calculation instance
+    """
+    # Relative run directory
+    directory = 'lmax43/locut20'
+
+    # Basis
+    l_max = {'ti': 4, 'o': 3}
+    lo_energy_cutoff = {'ti': {0: 20, 1: 20, 2: 20, 3: 20, 4: 20},
+                        'o': {0: 20, 1: 20, 2: 20, 3: 20}
+                        }
+    gw_basis = {}
+    for x in ['ti', 'o']:
+        gw_basis[x] = construct_optimised_basis(default_basis[x], lo_recommendations[x], l_max[x], lo_energy_cutoff[x])
+
+    # Submission script settings
+    run_script = set_run_script(default_directives, default_env_vars, default_module_envs)
+
+    return Calculation(directory, lo_energy_cutoff=lo_energy_cutoff, basis=gw_basis, run_script=run_script)
+
+
+def calc_lmax43_locut30(default_basis, lo_recommendations) -> Calculation:
+    # Relative run directory
+    directory = 'lmax43/locut30'
+
+    # Basis
+    l_max = {'ti': 4, 'o': 3}
+    lo_energy_cutoff = {'ti': {0: 30, 1: 30, 2: 30, 3: 30, 4: 30},
+                        'o': {0: 30, 1: 30, 2: 30, 3: 30}
+                        }
+    gw_basis = {}
+    for x in ['ti', 'o']:
+        gw_basis[x] = construct_optimised_basis(default_basis[x], lo_recommendations[x], l_max[x], lo_energy_cutoff[x])
+
+    run_script = set_run_script(default_directives, default_env_vars, default_module_envs)
+
+    return Calculation(directory, lo_energy_cutoff=lo_energy_cutoff, basis=gw_basis, run_script=run_script)
+
+
+def calc_lmax43_locut40(default_basis, lo_recommendations) -> Calculation:
+    # Relative run directory
+    directory = 'lmax43/locut40'
+
+    # Basis
+    l_max = {'ti': 4, 'o': 3}
+    lo_energy_cutoff = {'ti': {0: 40, 1: 40, 2: 40, 3: 40, 4: 40},
+                        'o': {0: 40, 1: 40, 2: 40, 3: 40}
+                        }
+    gw_basis = {}
+    for x in ['ti', 'o']:
+        gw_basis[x] = construct_optimised_basis(default_basis[x], lo_recommendations[x], l_max[x], lo_energy_cutoff[x])
+
+    run_script = set_run_script(default_directives, default_env_vars, default_module_envs)
+
+    return Calculation(directory, lo_energy_cutoff=lo_energy_cutoff, basis=gw_basis, run_script=run_script)
+
+
+def calc_lmax43_locut50(default_basis, lo_recommendations) -> Calculation:
+    # Relative run directory
+    directory = 'lmax43/locut50'
+
+    # Basis
+    l_max = {'ti': 4, 'o': 3}
+    lo_energy_cutoff = {'ti': {0: 50, 1: 50, 2: 50, 3: 50, 4: 50},
+                        'o': {0: 50, 1: 50, 2: 50, 3: 50}
+                        }
+    gw_basis = {}
+    for x in ['ti', 'o']:
+        gw_basis[x] = construct_optimised_basis(default_basis[x], lo_recommendations[x], l_max[x], lo_energy_cutoff[x])
+
+    run_script = set_run_script(default_directives, default_env_vars, default_module_envs)
+
+    return Calculation(directory, lo_energy_cutoff=lo_energy_cutoff, basis=gw_basis, run_script=run_script)
+
+
+def calc_lmax43_locut60(default_basis, lo_recommendations) -> Calculation:
+    # Relative run directory
+    directory = 'lmax43/locut60'
+
+    # Basis
+    l_max = {'ti': 4, 'o': 3}
+    lo_energy_cutoff = {'ti': {0: 60, 1: 60, 2: 60, 3: 60, 4: 60},
+                        'o': {0: 60, 1: 60, 2: 60, 3: 60}
+                        }
+    gw_basis = {}
+    for x in ['ti', 'o']:
+        gw_basis[x] = construct_optimised_basis(default_basis[x], lo_recommendations[x], l_max[x], lo_energy_cutoff[x])
+
+    run_script = set_run_script(default_directives, default_env_vars, default_module_envs)
+
+    return Calculation(directory, lo_energy_cutoff=lo_energy_cutoff, basis=gw_basis, run_script=run_script)
+
+
+def calc_lmax43_locut70(default_basis, lo_recommendations) -> Calculation:
+    # Relative run directory
+    directory = 'lmax43/locut70'
+
+    # Basis
+    l_max = {'ti': 4, 'o': 3}
+    lo_energy_cutoff = {'ti': {0: 70, 1: 70, 2: 70, 3: 70, 4: 70},
+                        'o': {0: 70, 1: 70, 2: 70, 3: 70}
+                        }
+    gw_basis = {}
+    for x in ['ti', 'o']:
+        gw_basis[x] = construct_optimised_basis(default_basis[x], lo_recommendations[x], l_max[x], lo_energy_cutoff[x])
+
+    run_script = set_run_script(default_directives, default_env_vars, default_module_envs)
+
+    return Calculation(directory, lo_energy_cutoff=lo_energy_cutoff, basis=gw_basis, run_script=run_script)
+
+
+def calc_lmax43_locut80(default_basis, lo_recommendations) -> Calculation:
+    # Relative run directory
+    directory = 'lmax43/locut80'
+
+    # Basis
+    l_max = {'ti': 4, 'o': 3}
+    lo_energy_cutoff = {'ti': {0: 80, 1: 80, 2: 80, 3: 80, 4: 80},
+                        'o': {0: 80, 1: 80, 2: 80, 3: 80}
+                        }
+    gw_basis = {}
+    for x in ['ti', 'o']:
+        gw_basis[x] = construct_optimised_basis(default_basis[x], lo_recommendations[x], l_max[x], lo_energy_cutoff[x])
+
+    run_script = set_run_script(default_directives, default_env_vars, default_module_envs)
+
+    return Calculation(directory, lo_energy_cutoff=lo_energy_cutoff, basis=gw_basis, run_script=run_script)
+
+
+def calc_lmax43_locut90(default_basis, lo_recommendations) -> Calculation:
+    # Relative run directory
+    directory = 'lmax43/locut90'
+
+    # Basis
+    l_max = {'ti': 4, 'o': 3}
+    lo_energy_cutoff = {'ti': {0: 90, 1: 90, 2: 90, 3: 90, 4: 90},
+                        'o': {0: 90, 1: 90, 2: 90, 3: 90}
+                        }
+    gw_basis = {}
+    for x in ['ti', 'o']:
+        gw_basis[x] = construct_optimised_basis(default_basis[x], lo_recommendations[x], l_max[x], lo_energy_cutoff[x])
+
+    run_script = set_run_script(default_directives, default_env_vars, default_module_envs)
+
+    return Calculation(directory, lo_energy_cutoff=lo_energy_cutoff, basis=gw_basis, run_script=run_script)
+
+
+def calc_lmax43_locut100(default_basis, lo_recommendations) -> Calculation:
+    # Relative run directory
+    directory = 'lmax43/locut100'
+
+    # Basis
+    l_max = {'ti': 4, 'o': 3}
+    lo_energy_cutoff = {'ti': {0: 100, 1: 100, 2: 100, 3: 100, 4: 100},
+                        'o': {0: 100, 1: 100, 2: 100, 3: 100}
+                        }
+    gw_basis = {}
+    for x in ['ti', 'o']:
+        gw_basis[x] = construct_optimised_basis(default_basis[x], lo_recommendations[x], l_max[x], lo_energy_cutoff[x])
+
+    run_script = set_run_script(default_directives, default_env_vars, default_module_envs)
+
+    return Calculation(directory, lo_energy_cutoff=lo_energy_cutoff, basis=gw_basis, run_script=run_script)
--- tio2/lo_spread/inputs.py
+++ tio2/lo_spread/inputs.py
@@ -1,53 +1,37 @@
 """ Generate inputs with several LO channels, and multiple LOs per channel
-
-Workflow Description:
-* Need to run a converged ground state, with lo recommendations as true.
-* Generate an input file for GW
-* With the existing basis, LO recommendations, and LO channel settings, build a new basis file.
-* Generate an appropriate submission script
-* Generate a directory name
-* Package this into a calculation type
-
- TODOs
-# Separate results processing from input generation
-# Functions:
-# Generate run directory name
-# Specify N LOs or cut-offs per channel
-# Get LOs in for the optimised basis (that are not already present in the default basis)
 """
 # External libs
 import os.path
 from pathlib import Path
-from typing import List, Optional, Tuple
+import shutil
+from typing import List
 
 # This package
 from src.inputs.gw import GWInput
 from src.parse.parsers import parse_lorecommendations, parse_species_xml
-from src.basis.optimised_basis import construct_optimised_basis
+from src.write.species import write_species_file_from_dict
+
 
 # TiO2 specific
 from tio2.lo_spread.ground_state_xml import converged_input_xml
-
-
-#  Variable (thing that varies between calculations) and directory.
-class Calculation:
-    def __init__(self, directory: str, variable: any, basis: Optional[dict] = None):
-        self.directory = directory
-        self.variable = variable
-        self.basis = basis
+from tio2.lo_spread.calculations import *
 
 
 def set_input() -> str:
     """ Compose converged ground state and GW input settings.
+
+    GW Fixed input
+    Ground state input read from python
+
     :return: Input xml string.
     """
     # Converged ground state
     input_xml = converged_input_xml.replace('do="skip"', 'do="fromfile"')
 
     # GW
-    gw = {'taskname': 'G0W0', 'nempty': 800, 'ngridq': [4, 4, 4], 'skipgnd': "false"}
+    gw = {'taskname': 'G0W0', 'nempty': 100, 'ngridq': [2, 2, 2], 'skipgnd': "false"}
     mixbasis = {'lmaxmb': 4, 'epsmb': 0.001, 'gmb': 1.0}
-    freqgrid = {'nomeg': 32, 'freqmax': 1.0}
+    freqgrid = {'nomeg': 16, 'freqmax': 1.0}
     barecoul = {'pwm': 2.0, 'stctol': 1e-16, 'barcevtol': 0.1}
     selfenergy = {'actype': 'pade', 'singularity': 'mpb'}
     gw_input = GWInput(gw=gw, mixbasis=mixbasis, freqgrid=freqgrid, barecoul=barecoul, selfenergy=selfenergy)
@@ -58,72 +42,57 @@ def set_input() -> str:
     return input_xml
 
 
-# So the annoying shit is I have to write this out for (4,4), (5,5) (7,7) and lo_energy_cutoff = [20, 40, 60, 80, 100, 120]
-# So that is 18 functions that barely differ - BUT this gives me total flexiblity if I want change what quantity I
-# vary OR exactly how I want to define the basis
-def calc_lmax55_locut150(default_basis, lo_recommendations) -> Calculation:
-    """ Define a calculation
+def main(root: str, ground_state_path: str, input_xml: str, calculations: List[Calculation]):
+    """ Main for running TiO2
 
-    Same cutoff for each channel.
-    :return:
+    Idea is to define the root, ground_state_path, input.xml and a list of calculation settings, then inject
+    into this routine. It's more code to define each calculation, but it's clean and robust.
+
+    Need some way of producing directory paths so I can easily load when I want to do post-processing?
+    - Should just be `full_directory` from above. All settings in calculations
     """
-    directory = 'lmax55/locut150'
-
-    lo_energy_cutoff = {'ti': {{0: 150, 1: 150, 2: 150, 3: 150, 4: 150, 5: 150}},
-                        'o': {{0: 150, 1: 150, 2: 150, 3: 150, 4: 150, 5: 150}},
-                        }
-
-    gw_basis = {}
-    for x in ['ti', 'o']:
-        gw_basis[x] = construct_optimised_basis(default_basis[x], lo_recommendations[x], lo_cutoff=lo_energy_cutoff[x])
-
-    return Calculation(directory, lo_energy_cutoff, basis=gw_basis)
-
-
-def main():
-    """ Main for running l_max = (5, 5) for TiO2
-    TODOs
-    OR number of LOs per l-channel (convert the nunber of LOs to a trial energy)
-
-    Parse existing basis
-    Add to it using LO recommendations
-    Write the new basis out
-
-    Idea is to define the root, input.xml and a list of calculation settings, then inject
-    to this routine. It's more code to define each calculation, but it's way cleaner and
-    more robust.
-
-    :return:
-    """
-    root = "/users/sol/abuccheri/wp2/tio2/lo_sweep/"
-
-    input_xml = set_input()
-    default_basis = parse_species_xml()
-    lo_recommendations = parse_lorecommendations("lorecommendations.dat", ['ti', 'o'], l_max=7, node_max=20)
-
-    calculations = [calc_lmax55_locut150(default_basis, lo_recommendations)]
+    osjp = os.path.join
 
     for calculation in calculations:
-        # Create the directories
-        full_directory = os.path.join(root, calculation.directory)
+        # Create run directory
+        full_directory = osjp(root, calculation.directory)
         Path(full_directory).mkdir(parents=True, exist_ok=True)
 
         # Copy STATE.OUT from the ground state
+        shutil.copyfile(osjp(ground_state_path, "STATE.OUT"), osjp(full_directory, "STATE.OUT"))
 
         # Input xml
-        with open(os.path.join(full_directory, "input.xml"), "w") as fid:
+        with open(osjp(full_directory, "input.xml"), "w") as fid:
             fid.write(input_xml)
 
         # Species files
         for x in ['ti', 'o']:
-            species_fname = f'{x}.xml'.capitalize()
-            with open(os.path.join(full_directory, species_fname), "w") as fid:
-                fid.write(calculation.basis[x])
+            file_name = osjp(calculation.directory, x.capitalize() + '.xml')
+            write_species_file_from_dict(calculation.basis[x], file_name=file_name)
 
         # Run script
+        with open(osjp(full_directory, "run.sh"), "w") as fid:
+            fid.write(calculation.run_script)
 
-        # Need some way of producing directory paths so I can easily load when I want
-        # to do post-processing.
 
+if __name__ == "__main__":
+    root = "/Users/alexanderbuccheri/exgw/test/tio2/lo_sweep/"
 
-main()
+    ground_state_path = "/Users/alexanderbuccheri/exgw/test/tio2/ground_state"
+    default_basis = {}
+    default_basis['ti'] = parse_species_xml("ref/Ti.xml")
+    default_basis['o'] = parse_species_xml("ref/O.xml")
+    lo_recommendations = parse_lorecommendations("ref/lorecommendations.dat", ['ti', 'o'], l_max=7, node_max=20)
+
+    input_xml = set_input()
+    calculations = [calc_lmax43_locut20(default_basis, lo_recommendations),
+                    calc_lmax43_locut30(default_basis, lo_recommendations),
+                    calc_lmax43_locut40(default_basis, lo_recommendations),
+                    calc_lmax43_locut50(default_basis, lo_recommendations),
+                    calc_lmax43_locut60(default_basis, lo_recommendations),
+                    calc_lmax43_locut70(default_basis, lo_recommendations),
+                    calc_lmax43_locut80(default_basis, lo_recommendations),
+                    calc_lmax43_locut90(default_basis, lo_recommendations),
+                    calc_lmax43_locut100(default_basis, lo_recommendations)]
+
+    main(root, ground_state_path, input_xml, calculations)
